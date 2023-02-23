ad8ab29e593722f30d72bad9ef7b36e2fe217f98
kubawieczorek <jwieczorek94@wp.pl>
2023-02-23 19:55:56 +0100

    Update Main.java

--- test-kodilla/src/main/java/org/example/Main.java
+++ test-kodilla/src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
         for(int i = 0; i < 100; i++){
             System.out.println(i);
         }
-        for(int i = 0; i < 100; i++){
+        for(int i = 0; i < 10; i++){
             System.out.println(i);
         }
     }
