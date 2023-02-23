ad688eb52dfabd6d785a4cccbb846000e87765ac
kubawieczorek <jwieczorek94@wp.pl>
2023-02-23 19:32:31 +0100

    Update Main.java

--- test-kodilla/src/main/java/org/example/Main.java
+++ test-kodilla/src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!!!");
 
-        for(int i = 0; i < 100; i++){
+        for(int i = 0; i < 1000; i++){
             System.out.println(i);
         }
     }
